edb44178f8ef4dc75c550735479531718c4d1894
Ash Whiting <ash@reddico.co.uk>
2019-06-20 14:42:58 +0100

    Minor amend

--- plugin.php
+++ plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: test-block — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: test-block — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: Block Kit
+ * Plugin URI: 
+ * Description: Custom Block Kit
+ * Author: Ash Whiting
+ * Author URI: https://ashwhiting.com
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
