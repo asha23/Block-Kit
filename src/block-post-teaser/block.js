b18fc97803347487babbf86dae6be8687833fd61
Ash Whiting <ash@reddico.co.uk>
2019-06-20 15:57:17 +0100

    post teaser block

--- src/block-post-teaser/block.js
+++ src/block-post-teaser/block.js
@@ -1,5 +1,5 @@
 /**
- * BLOCK: test-block
+ * BLOCK: Post Select
  *
  * Registering a basic block with Gutenberg.
  * Simple block, renders and saves the same content without any interactivity.
@@ -12,8 +12,8 @@ import './editor.scss';
 
 const { __ } = wp.i18n; 
 const { registerBlockType, InspectorControls } = wp.blocks; 
-const {SelectControl} = wp.components;
-const {Component} = wp.element
+const { SelectControl } = wp.components;
+const { Component } = wp.element
 
 class selectPosts extends Component {
 	static getInitialState(selectedPost) {
@@ -22,6 +22,21 @@ class selectPosts extends Component {
 			selectedPost: selectedPost,
 			post: {}
 		}
+	}
+	
+	onChangeSelectPost(value) {
+		const post = this.state.posts.find((item) => {
+			return item.id == parseInt(value)
+		})
+		
+		this.setState ({selectedPost: parseInt(value), post})
+		
+		this.props.setAttributes({
+			selectedPost: parseInt(value),
+			title: post.title.rendered,
+			content: post.excerpt.rendered,
+			link: post.link
+		})
 	}
 	
 	getOptions() {
@@ -34,7 +49,7 @@ class selectPosts extends Component {
 			} else {
 				this.setState(posts);
 			}
-		})
+		});
 	}
 	
 	constructor() {
@@ -42,17 +57,35 @@ class selectPosts extends Component {
 		this.state = this.constructor.getInitialState(this.props.attributes.selectedPost)
 		this.getOptions = this.getOptions.bind(this);
 		this.getOptions();
-	}
+		
+		this.onChangeSelectPost = this.onChangeSelectPost.bind(this);
+	};
 	
 	render() {
 		let options = [{value:0, label: __('Select a post')}];
-		let output = __('Loading post');
+		let output = __('Loading posts');
+		this.props.className += ' loading';
+		if (this.state.posts.length > 0) {
+			
+		} else {
+			output = __('No posts found in the system');
+		}
+		if(this.state.post.hasOwnProperty('title')) {
+			output = <div className="post">
+				<a href="">
+					<p dangerouslySetInnerHTML={{__html: this.state.post.excerpt.rendered}}></p>
+				</a>
+			</div>;
+			this.props.className += ' has-post';
+		} else {
+			this.props.className += ' no-post';
+		}
 		if (this.state.posts.forEach((post) => {
 			options.push({
 				value:post.id, 
 				label:post.title.rendered
-			})
-		}))
+			});
+		}));
 		return [
 			!! this.props.isSelected && (
 				<InspectorControls key='inspector'>
@@ -60,7 +93,8 @@ class selectPosts extends Component {
 									label={__('Select a Post')} 
 									options={options} />
 				</InspectorControls>
-			)
+			),
+			<div className={this.props.className}>{output}</div>
 		]
 	}
 }
@@ -78,7 +112,7 @@ registerBlockType( 'ash/post-teaser', {
 	
 	attributes: {
 		content: {
-			type: array,
+			type: 'array',
 			source: 'children',
 			selector: 'p'
 		},
@@ -95,5 +129,18 @@ registerBlockType( 'ash/post-teaser', {
 			default:0
 		}
 	},
-	 
-} );
+	
+	save: function(props) {
+		return (
+			<div className={props.className}>
+				<div className="post">
+					<a href={props.attributes.link}>
+						<h2 dangerouslySetInnerHTML={{__html:props.attributes.content}}>
+							
+						</h2>
+					</a>
+				</div>
+			</div>
+		)
+	}
+});
